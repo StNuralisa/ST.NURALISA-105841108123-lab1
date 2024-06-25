91d1efeee4dc49271915b8188befcb6e7fbc0e61
StNuralisa <150004163+StNuralisa@users.noreply.github.com>
2024-06-25 20:38:54 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function changeContent() {
+    var element = document.getElementById("content");
+    element.innerHTML = "lopyuuuu";
+    document.getElementById("awwch").src = "https://i.pinimg.com/564x/40/d5/5f/40d55f332ece52c0b480e1337eaaed56.jpg" ;
+}
